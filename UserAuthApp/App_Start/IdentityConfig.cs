a5e7b9d93d843a927837f5262e5c0707da08541a
Darren Ruane <Darzer@users.noreply.github.com>
2016-04-18 20:32:49 +0100

    Added a requirement for users email confirmation before they can login.

--- UserAuthApp/App_Start/IdentityConfig.cs
+++ UserAuthApp/App_Start/IdentityConfig.cs
@@ -31,7 +31,7 @@ namespace UserAuthApp
             var myMessage = new SendGridMessage();
             myMessage.AddTo(message.Destination);
             myMessage.From = new System.Net.Mail.MailAddress(
-                                "x00090469@ittd.ie", "Darren R.");
+                                "X00090469@Ittd.ie", "Darren R.");
             myMessage.Subject = message.Subject;
             myMessage.Text = message.Body;
             myMessage.Html = message.Body;
