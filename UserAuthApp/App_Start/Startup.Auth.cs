509e952927530b3625ee21b03bf75bc643bcdff6
Darren Ruane <Darzer@users.noreply.github.com>
2016-04-19 20:19:23 +0100

    Added images for social account login links. Added Microsoft and Twitter login links.

--- UserAuthApp/App_Start/Startup.Auth.cs
+++ UserAuthApp/App_Start/Startup.Auth.cs
@@ -6,6 +6,8 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using UserAuthApp.Models;
+using Microsoft.Owin.Security.Twitter;
+using Microsoft.Owin.Security;
 
 namespace UserAuthApp
 {
@@ -34,7 +36,7 @@ namespace UserAuthApp
                         validateInterval: TimeSpan.FromMinutes(30),
                         regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
                 }
-            });            
+            });
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
             // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
@@ -46,13 +48,24 @@ namespace UserAuthApp
             app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
 
             // Uncomment the following lines to enable logging in with third party login providers
-            //app.UseMicrosoftAccountAuthentication(
-            //    clientId: "",
-            //    clientSecret: "");
+            app.UseMicrosoftAccountAuthentication(
+               clientId: System.Configuration.ConfigurationManager.AppSettings["MSClientID"],
+               clientSecret: System.Configuration.ConfigurationManager.AppSettings["MSClientSecret"]);
 
-            //app.UseTwitterAuthentication(
-            //   consumerKey: "",
-            //   consumerSecret: "");
+            app.UseTwitterAuthentication(new TwitterAuthenticationOptions
+            {
+                ConsumerKey = System.Configuration.ConfigurationManager.AppSettings["ConsumerKey"],
+                ConsumerSecret = System.Configuration.ConfigurationManager.AppSettings["ConsumerSecret"],
+                BackchannelCertificateValidator = new CertificateSubjectKeyIdentifierValidator(new[]
+                 {
+                     "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA - G2
+                     "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA - G3
+                     "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary Certification Authority - G5
+                     "39A55D933676616E73A761DFA16A7E59CDE66FAD", // Symantec Class 3 Secure Server CA - G4
+                     "5168FF90AF0207753CCCD9656462A212B859723B", //DigiCert SHA2 High Assurance Server C‎A 
+                     "B13EC36903F8BF4701D498261A0802EF63642BC3" //DigiCert High Assurance EV Root CA
+                 })
+            });
 
             app.UseFacebookAuthentication(
                appId: System.Configuration.ConfigurationManager.AppSettings["AppID"],
---
+++ UserAuthApp/Migrations/201604191729568_Init1.cs
@@ -0,0 +1,16 @@
+namespace UserAuthApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Init1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
